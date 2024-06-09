e420bf921df87025d0a3bb261770ba254acf7290
Kulpot <kulpotwahu@gmail.com>
2024-06-10 06:10:18 +0800

    //Floating Point Exponent and Mantissa  //https://www.youtube.com/watch?v=nkhZA3JNClc&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N&index=2

--- Floating Point Exponent and Mantissa/CppScratchPad.cpp
+++ Floating Point Exponent and Mantissa/CppScratchPad.cpp
@@ -2,6 +2,8 @@
 using std::cout;
 using std::endl;
 
+//Floating Point Exponent and Mantissa  //https://www.youtube.com/watch?v=nkhZA3JNClc&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N&index=2
+
 void main()
 {
 	// 654.321
